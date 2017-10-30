8017fc598eddf644093521a3aa5eb1ff46725a1d
Erik Rasmussen <erasmussen@xebialabs.com>
2017-10-30 10:19:10 -0600

    Initial Implementation
    Created task to spawn sub-releases with several options. Provide the templates,
    the release titles, and release variables. Additionally provide options to
    specify whether the releases should be launched in parallel, and also whether
    or not to create corresponding gates for the sub-releases.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![Build Status](https://travis-ci.org/erasmussen39/xlr-utilities-plugin.svg?branch=master)](https://travis-ci.org/erasmussen39/xlr-utilities-plugin)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/c004d5eaa7544b6d8f50844f2c46e56e)](https://www.codacy.com/app/erasmussen39/xlr-utilities-plugin?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=erasmussen39/xlr-utilities-plugin&amp;utm_campaign=Badge_Grade)
 [![Maintainability](https://api.codeclimate.com/v1/badges/797d2a29a35f9f129c8e/maintainability)](https://codeclimate.com/github/erasmussen39/xlr-utilities-plugin/maintainability)
-[![License: MIT][xlr-utilities-plugin-license-image] ][xlr-utilities-plugin-license-url]
+[![License: MIT][xlr-utilities-plugin-license-image]][xlr-utilities-plugin-license-url]
 [![Github All Releases][xlr-utilities-plugin-downloads-image]]()
 
 [xlr-utilities-plugin-license-image]: https://img.shields.io/badge/License-MIT-yellow.svg
@@ -11,9 +11,25 @@
 [xlr-utilities-plugin-downloads-image]: https://img.shields.io/github/downloads/xebialabs-community/xlr-utilities-plugin/total.svg
 
 ## Preface
-
-## Overview
+This document describes the functionality provided by the xlr-utilities-plugin.
 
 ## Installation
+Copy the plugin JAR file into the SERVER_HOME/plugins directory of XL Release.
 
-## References
+## Overview
+This plugin is intended to provide a variety of useful utility tasks for XL Release.
+![Generate Sub-Releases](images/generate_sub_releases.png)
+## Tasks
+#### utilties.GenerateSubReleases
+Task used to dynamically generate Sub-Releases.
+###### Release Templates
+The list of release templates used to create the sub-releases (required).
+###### Release Titles
+The list of release titles to be used when creating the sub-releases (optional). If none are provided the template title will be utilized.
+###### Create Gates
+Boolean parameter determining whether a gate task will be created for each sub-release that is spawned.
+###### Parallel
+Boolean parameter determining whether or not the spawned sub-releases will be run in parallel.
+###### Release Variables
+The release variables that should be passed to each sub-release. Currently, only String variables are supported. The list should be in the following format:
+`KEY1=VAL1,KEY2=VAL2`
---
+++ src/main/resources/utilities/Client.py
@@ -0,0 +1,98 @@
+    #
+# Copyright 2017 XEBIALABS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from com.xebialabs.xlrelease.domain.variables import StringVariable
+
+class Client(object):
+    def __init__(self):
+        return
+
+    @staticmethod
+    def get_client():
+        return Client()
+
+    @staticmethod
+    def getCurrentPhase(variables):
+        current_phase = variables['getCurrentPhase']
+        return current_phase()
+
+    @staticmethod
+    def find_variable_with_key(key, variables):
+        for variable in variables:
+            if variable.getKey() == key:
+                return variable
+
+    @staticmethod
+    def find_template_with_name(name, variables):
+        templates = variables['templateApi'].getTemplates(name)
+        return templates[0]
+
+    @staticmethod
+    def add_task(variables, container, task_type, title):
+        task = variables['taskApi'].newTask(task_type)
+        task.title = title
+        return variables['taskApi'].addTask(container, task).getId()
+
+    @staticmethod
+    def generate_create_release_task(variables, release_title, template_name, iteration):
+        task = variables['taskApi'].newTask("xlrelease.CreateReleaseTask")
+        task.title = release_title
+        task.newReleaseTitle = release_title
+        task.templateId = "%s" % Client.find_template_with_name(template_name, variables).getId()
+        task.createdReleaseId = "${RELEASE_ID%s}" % iteration
+        if len(variables['release_variables']) > iteration:
+            release_vars = []
+            for variable in variables['release_variables'][iteration].split(','):
+                key_value = variable.split("=")
+                v = StringVariable()
+                v.setKey(key_value[0])
+                v.setValue(key_value[1])
+                release_vars.append(v)
+            task.templateVariables = release_vars
+        return task
+
+    @staticmethod
+    def generate_gate_task(variables, release_title, gates_container, iteration):
+        task = variables['taskApi'].newTask("xlrelease.GateTask")
+        task.title = "Wait For Completion of %s" % release_title
+        gate_id = variables['taskApi'].addTask(gates_container, task).getId()
+        variables['taskApi'].addDependency(gate_id, "${RELEASE_ID%s}" % iteration)
+
+    @staticmethod
+    def generate_create_sub_releases(variables, releases_container, gates_container):
+        tasks = []
+        release_templates = variables['release_templates']
+        release_titles = variables['release_titles']
+        i = 0
+        for template_name in release_templates:
+            if (len(release_titles) <= i) or not release_titles[i]:
+                release_title = template_name
+            else:
+                release_title = release_titles[i]
+            task = Client.generate_create_release_task(variables, release_title, template_name, i)
+            tasks.append(variables['taskApi'].addTask(releases_container, task).getId())
+            if variables['create_gates']:
+                Client.generate_gate_task(variables, release_title, gates_container, i)
+            i += 1
+        return tasks
+
+    @staticmethod
+    def generate_subrelease(variables):
+        releases_container = Client.getCurrentPhase(variables).getId()
+        gates_container = Client.getCurrentPhase(variables).getId()
+        if variables['parallel']:
+            releases_container = Client.add_task(variables, Client.getCurrentPhase(variables).getId(), "xlrelease.ParallelGroup", "Start Releases")
+            gates_container = Client.add_task(variables, Client.getCurrentPhase(variables).getId(), "xlrelease.ParallelGroup", "Wait For Releases")
+        return Client.generate_create_sub_releases(variables, releases_container, gates_container)
+
+    @staticmethod
+    def utilities_generatesubreleases(variables):
+        task_ids = Client.generate_subrelease(variables)
+        return {"output" : "%s" % task_ids}
